2589aebc42a94d42d610d3bd93b6e68509c62b97
Niels Verdickt <niels.verdickt@outlook.com>
2022-03-03 11:54:01 +0100

    feat: add eventlisteners

--- Lab 3/main.js
+++ Lab 3/main.js
@@ -1,5 +1,8 @@
 import './style.css'
 
+import App from "./classes/app.js";
+const app = new App();
+
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
